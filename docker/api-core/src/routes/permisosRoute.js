abaa3e234666fdaaa69cead32bb481ac9e7948ad
alejandro-f-d <alejandro.fisac.contact@gmail.com>
2026-02-16 19:48:01 +0100

    Added libraries for jwt, change env-example, added documentation for getPermisos.

--- docker/api-core/src/routes/permisosRoute.js
+++ docker/api-core/src/routes/permisosRoute.js
@@ -2,5 +2,21 @@ import express from "express";
 const router = express.Router();
 import { getAllPermisos } from '../controller/permisosController.js'
 
+
+
+/**
+ * @swagger
+ * /api/permisos:
+ *   get:
+ *     summary: Obtienes todos los permisos.
+ *     tags: [permisos]
+ *     responses:
+ *       200:
+ *         description: Devuelve el listado de todos los roles existentes.
+ *       500:
+ *         description: Error interno del servidor.
+ */
+
+
 router.get("/", getAllPermisos);
 export default router;
