4b89d5364d51b6cc805253e966c881bc04ab9277
Alejandro <alexfisac04@gmail.com>
2026-03-02 22:58:00 +0100

    Intento redis 1

--- docker/script_test.sh
+++ docker/script_test.sh
@@ -1,4 +1,5 @@
 docker compose down -v --remove-orphans
 docker rmi medal-gestion-sistema-medal-api-core:latest
 docker compose up -d medal-bdd-postgres
+docker compose up -d redis 
 docker compose up --build medal-api-core
