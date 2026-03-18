421c9302e841dc856888bc9194b9374b027b51ce
Alejandro <alexfisac04@gmail.com>
2026-03-18 19:08:40 +0100

    Update del script de inicialización. Manual de administrador del backend.

--- docker/script.sh
+++ docker/script.sh
@@ -2,8 +2,9 @@ docker compose down -v --remove-orphans
 docker rmi medal-gestion-sistema-medal-api-core:latest
 docker rmi medal-gestion-sistema-medal-worker:latest
 docker compose up -d medal-bdd-postgres
-docker compose up -d redis 
+docker compose up -d redis
 docker compose up -d medal-worker
 docker compose up -d medal-storage
 docker compose up -d medal-sign-validator
+docker compose up -d medal-nginx
 docker compose up --build medal-api-core
